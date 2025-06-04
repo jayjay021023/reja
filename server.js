488171d6ba9c063911e1a13afb3616ee50e57d22
jayjay021023 <azizaesonova767@gmail.com>
2025-06-04 16:55:26 +0500

    feat: develop author portfolio page

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const app = express();
 const http = require("http");
 const fs = require("fs");
 
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) =>{
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
+
 //1 kirish kodlari
 //expressga kirib kelayotgan malumotlarga bogliq bolgan codlar yoziladi
 app.use(express.static("public"));
@@ -32,5 +41,5 @@ app.get("/", function (req , res) {
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function() {
-    console.log(`THe server is running successfully on port: ${PORT}`);
+    console.log(`The server is running successfully on port: ${PORT}`);
 })
