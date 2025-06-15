8bee3bb9dbb395f01e54fcc56ba45e9a9527dc73
jayjay021023 <azizaesonova767@gmail.com>
2025-06-15 14:14:02 +0500

    feat: reja loyihamizning yakuniy bosqichi logic

--- public/browser.js
+++ public/browser.js
@@ -62,6 +62,29 @@ if(e.target.classList.contains("delete-me")) {
 
 //edit oper
 if(e.target.classList.contains("edit-me")) {
-    alert("siz edit tugmasini bosdingiz!")
+    let userInput = prompt("o'zgartirishni kiriting",
+       e.target.parentElement.parentElement.querySelector(".item-text").innerHTML
+  );
+    if(userInput) {
+      axios.post("/edit-item", {
+        id: e.target.getAttribute("data-id"),
+         new_input: userInput,
+        }).then((response) => {
+          console.log(response.data);
+          e.target.parentElement.parentElement.querySelector(".item-text"
+
+          ).innerHTML = userInput;
+        })
+        .catch((err) => {
+          console.log("iltimos qaytadan harakat qiling");
+        })
+    }
 }
 });
+
+document.getElementById("clean-all").addEventListener("click", function() {
+axios.post("/delete-all", {delete_all:true}).then(respose => {
+  alert(respose.data.state);
+  document.location.reload();
+})
+})
